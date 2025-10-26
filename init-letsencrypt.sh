a586f8205703d3a7c5ebf6a97d10ea608f62d99e
root <root@vultr.guest>
2025-10-26 07:09:52 +0000

    fix: change ssl domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(www.viocare.kr viocare.kr) # TODO 도메인 입력
+domains=(api.waterregen.kr) # TODO 도메인 입력
 
 rsa_key_size=4096
 data_path="./data/certbot"
