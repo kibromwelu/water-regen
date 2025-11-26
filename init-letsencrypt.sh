ac1743e9a05cf94afdc727c3e38c3ff0a3a87489
lebania <lebania@sweech.io>
2025-11-26 11:15:23 +0900

    fix: Deploy

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,13 +5,13 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(www.waterregen.kr waterregen.kr api.waterregen.kr)
+domains=(www.waterregen.kr waterregen.kr)
 
 rsa_key_size=4096
 data_path="./data/certbot"
 
 email="dev@sweech.io"
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits, set 0 for production env.
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits, set 0 for production env.
 
 echo "List of domains(s) :"
 for domain in ${domains[@]}; do
